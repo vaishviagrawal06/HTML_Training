2bdfdcd05b4e93dbd5fdf00feace9d0a5a11d0a4
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-19 15:23:44 +0530

    linked list

--- dsa_training/linkedlist/hello.java
+++ dsa_training/linkedlist/hello.java
@@ -1,4 +1,29 @@
-class node{
+class Node {
     int data;
-    node next;
+    Node next;
+
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+    public class Main {
+    public static void main(String[] args) {
+
+        Node first = new Node(10);   
+        System.out.println(first.data);  
+    }
+}
+public class hello{
+    public static void main(String[] args) {
+
+        Node first = new Node(10);
+        Node second = new Node(20);
+
+        first.next = second;   
+
+        System.out.println(first.data);        
+        System.out.println(first.next.data);   
+    }
+}
+
 }
