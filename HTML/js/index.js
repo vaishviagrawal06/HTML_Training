f9420424050817ee7141e20432c02548c8aed90d
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-23 11:06:20 +0530

    js

---
+++ HTML/js/index.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+let btn = document.querySelector('button')
+let body = document.querySelector('body')
+
+btn.addEventListener("click",function(){
+    body.style.backgroundcolor='blue'
+})
