5204c3d8299a0075c202b0f7fcc6961bbe074191
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-27 21:51:28 +0530

    snake game

--- HTML/js1/index.js
+++ HTML/js1/index.js
@@ -1,69 +1,76 @@
-let canvas = document.querySelector("canvas");
-let pen = canvas.getContext("2d");
+let canvas=   document.querySelector("canvas")
+  let pen=  canvas.getContext("2d")
+   pen.fillStyle='gold'
+   let cell=50
+   let cellQ=[[0,0]]
+   let gameOver=false
+   let direction='right'
 
-pen.fillStyle = 'yellow';
-
-let cell = 50;
-let cellQ = [[0, 0]];
-let direction = "right"; 
-
-document.addEventListener("keydown", function (e) {
-
-    if (e.key == 'ArrowDown') {
-        direction = 'down';
-    } 
-    else if (e.key == 'ArrowUp') {
-        direction = 'up';
-    }
-    else if (e.key =='ArrowLeft') {
-        direction = 'left';
-    }
-    else if (e.key == 'ArrowRight') {
-        direction = 'right';
-    }
-});
-
-function draw() {
-    pen.clearRect(0, 0, 1000, 600);
-    for (let i of cellQ) {
-        pen.fillRect(i[0], i[1], cell, cell);
-    }
-}
-
-function update() {
-    let x = cellQ[cellQ.length - 1][0];
-    let y = cellQ[cellQ.length - 1][1];
-
-    let newX = x;
-    let newY = y;
-
-    if (direction == 'right') {
-        newX = x + cell;
-        newY=y
-        if(newX==700){
-            gameOver=true;
+ let id=  setInterval(()=>{
+    draw()
+    update()
+   },500)
+     document.addEventListener("keydown",function(e){
+        // console.log(e);
+        if(e.key=='ArrowDown'){
+            direction='down'
+        }else if(e.key=='ArrowUp'){
+            direction='up'
+        }else if(e.key=='ArrowLeft'){
+            direction='left'
         }
-    }
-    else if (direction == 'left') {
-        newX = x - cell;
-        if(newX==700){
-            gameOver=true;
+        else{
+            direction='right'
         }
+     })
+
+   function draw(){
+    // for()
+    if(gameOver==true){
+        clearInterval(id)
+        return;
     }
-    else if (direction == 'up') {
-        newY = y - cell;
-    }
-    else if (direction == 'down') {
-        newY = y + cell;
+    pen.clearRect(0,0,1000,600)
+    for(let i of cellQ){
+        pen.fillRect(i[0],i[1],cell,cell)
     }
 
-    cellQ.push([newX, newY]);
-    cellQ.shift();
-}
+   }
 
 
-
-setInterval(() => {
-    update();
-    draw();
-}, 300);
+   function update(){
+ let x=   cellQ[cellQ.length-1][0]
+ let y=   cellQ[cellQ.length-1][1]
+ let newX
+ let nexY
+ if(direction=='right'){
+    newX=x+cell;
+    nexY=y
+    if(newX==700){
+        gameOver=true
+    }
+ }
+ else if(direction=='left'){
+    newX=x-cell;
+    nexY=y
+    if(newX<0){
+        gameOver=true
+    }
+ }
+ else if(direction=='down'){
+    newX=x
+    nexY=y+cell
+    if(nexY==400){
+        gameOver=true
+    }
+ }
+ else{
+    newX=x
+    nexY=y-cell
+    if(nexY<0){
+        gameOver=true
+    }
+ }
+ cellQ.push([newX,nexY])
+ cellQ.shift()
+   }
