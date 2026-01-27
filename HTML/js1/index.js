f20c4f7ac8f7b4f15805ca553c49a2cc7e0aec74
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-27 11:55:08 +0530

    snake game

--- HTML/js1/index.js
+++ HTML/js1/index.js
@@ -1,5 +1,13 @@
-let canvas = document.querySelector(canvas)
+let canvas = document.querySelector("canvas")
 
 let pen=canvas.getContext("2d")
 pen.fillStyle ='yellow'
-pen.fillRect(0,0,30,30)
+// pen.fillRect(0,0,30,30)
+let cell=50;
+let cellQ=[[0,0]]
+
+function draw(){
+    for(let i of cellQ){
+        pen.fillRect(i[0],i[i],cell,cell)
+    }
+}
