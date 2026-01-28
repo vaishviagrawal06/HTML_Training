d80539f6969413e15712f9458a85ca48f7711883
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-28 22:01:12 +0530

    snakegame

--- HTML/js1/index.js
+++ HTML/js1/index.js
@@ -1,103 +1,103 @@
+let canvas = document.querySelector("canvas")
+let pen = canvas.getContext("2d")
+pen.fillStyle = 'gold'
+let count = 0;
+let cell = 50
+let cellQ = [[0, 0]]
+let gameOver = false
+let direction = 'right'
+let randomC = generateRandomCell()
 
-   let canvas=   document.querySelector("canvas")
-  let pen=  canvas.getContext("2d")
-   pen.fillStyle='gold'
-   let count=0;
-   let cell=50
-   let cellQ=[[0,0]]
-   let gameOver=false
-   let direction='right'
-   let randomC=generateRandomCell()
-
- let id=  setInterval(()=>{
+let id = setInterval(() => {
     draw()
     update()
-   },200)
-     document.addEventListener("keydown",function(e){
-       
-        if(e.key=='ArrowDown'){
-            direction='down'
-        }else if(e.key=='ArrowUp'){
-            direction='up'
-        }else if(e.key=='ArrowLeft'){
-            direction='left'
-        }
-        else{
-            direction='right'
-        }
-     })
+}, 200)
+document.addEventListener("keydown", function (e) {
 
-   function draw(){
-   
-    if(gameOver==true){
+    if (e.key == 'ArrowDown') {
+        direction = 'down'
+    } else if (e.key == 'ArrowUp') {
+        direction = 'up'
+    } else if (e.key == 'ArrowLeft') {
+        direction = 'left'
+    }
+    else {
+        direction = 'right'
+    }
+})
+
+function draw() {
+
+    if (gameOver == true) {
         clearInterval(id)
-        pen.fillStyle='snow'
-        pen.fillText("Game Over" ,100,300)
+        pen.fillStyle = 'snow'
+        pen.fillText("Game Over", 100, 300)
         return;
     }
-       pen.fillStyle='red'
-    pen.clearRect(0,0,1000,600)
-    for(let i of cellQ){
-        pen.fillRect(i[0],i[1],cell,cell)
+    pen.fillStyle = 'red'
+    pen.clearRect(0, 0, 1000, 600)
+    for (let i of cellQ) {
+        pen.fillRect(i[0], i[1], cell, cell)
     }
-       pen.font='20px sans-sarif'
-        pen.fillText(`Score ${count}`,40,100)
-    pen.fillStyle='green'
-    pen.fillRect(randomC[0],randomC[1],cell,cell)
+    pen.font = '20px sans-sarif'
+    pen.fillText(`Score ${count}`, 40, 100)
+    pen.fillStyle = 'green'
+    pen.fillRect(randomC[0], randomC[1], cell, cell)
 
 
-   }
+}
 
 
-   function update(){
- let x=   cellQ[cellQ.length-1][0]
- let y=   cellQ[cellQ.length-1][1]
- let newX
- let nexY
- if(direction=='right'){
-    newX=x+cell;
-    nexY=y
-    if(newX==700){
-        gameOver=true
+function update() {
+    let x = cellQ[cellQ.length - 1][0]
+    let y = cellQ[cellQ.length - 1][1]
+    let newX
+    let nexY
+    if (direction == 'right') {
+        newX = x + cell;
+        nexY = y
+        if (newX == 700) {
+            gameOver = true
+        }
     }
- }
- else if(direction=='left'){
-    newX=x-cell;
-    nexY=y
-    if(newX<0){
-        gameOver=true
+    else if (direction == 'left') {
+        newX = x - cell;
+        nexY = y
+        if (newX < 0) {
+            gameOver = true
+        }
     }
- }
- else if(direction=='down'){
-    newX=x
-    nexY=y+cell
-    if(nexY==400){
-        gameOver=true
+    else if (direction == 'down') {
+        newX = x
+        nexY = y + cell
+        if (nexY == 400) 
+        {
+            gameOver = true
+        }
     }
- }
- else{
-    newX=x
-    nexY=y-cell
-    if(nexY<0){
-        gameOver=true
+    else {
+        newX = x
+        nexY = y - cell
+        if (nexY < 0) {
+            gameOver = true
+        }
     }
- }
 
- if(newX==randomC[0]  && nexY==randomC[1]){
-    randomC=generateRandomCell()
-    count=count+1;
+    if (newX == randomC[0] && nexY == randomC[1]) {
+        randomC = generateRandomCell()
+        count = count + 1;
 
- }
- else{
-    cellQ.shift()
+    }
+    else {
+        cellQ.shift()
 
- }
- cellQ.push([newX,nexY])
+    }
+    cellQ.push([newX, nexY])
 
-   }
-     function generateRandomCell(){
-        return[
-            Math.floor(Math.random()*650/50)*50,
-            Math.floor(Math.random()*350/50)*50
-        ]
-     }
+}
+function generateRandomCell() {
+    return [
+        Math.floor(Math.random() * 650 / 50) * 50,
+        Math.floor(Math.random() * 350 / 50) * 50
+    ]
+}
